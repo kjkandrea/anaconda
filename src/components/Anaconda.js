c2cbf8103b62924431a69f1b653d8067a7462ddc
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-08-12 20:28:20 +0900

    remove Item #1

--- src/components/Anaconda.js
+++ src/components/Anaconda.js
@@ -1,6 +1,7 @@
 const Anaconda = {}
 
 Anaconda.setup = function(el) {
+  this.rootEl = el
   this.el = {
     head: el.querySelector('#head-items'),
     body: el.querySelector('#body-items'),
@@ -8,6 +9,7 @@ Anaconda.setup = function(el) {
   }
 
   this.render()
+  this.bindEvents()
 }
 
 Anaconda.render = function() {
@@ -24,12 +26,29 @@ Anaconda.render = function() {
   for(const part in Anaconda.el) {
     let template = ''
 
-    data[part].forEach(e => {
-      template += `<li class="Box-row">${e.data}</li>`
+    data[part].forEach((e, idx) => {
+      template += `
+      <li class="todo-item Box-row d-flex flex-items-center" data-part="${part}" data-index="${idx}">
+        <div class="flex-auto">${e.data}</div>
+        <button class="btn btn-sm btn-remove">🗑️</button>
+      </li>`
     })
 
     Anaconda.el[part].innerHTML = template
   }
+}
+
+Anaconda.bindEvents = function() {
+  const target = this.rootEl.querySelectorAll('.js-items')
+  target.forEach(el => {
+    el.addEventListener('click', e => {
+      if(e.target.tagName === 'BUTTON') {
+        const part = e.target.closest('li').dataset.part
+        const index = e.target.closest('li').dataset.index
+        console.log('%s 파트 %s 번째 아이템 remove click event', part, index)
+      }
+    })
+  })
 }
 
 export default Anaconda
